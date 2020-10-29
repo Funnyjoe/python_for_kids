5eefd8cbaaf9653f85c66085fb95b1ea692a6630
Funnyjoe <38206938+Funnyjoe@users.noreply.github.com>
2020-10-29 18:15:28 +0800

    p42_v2

---
+++ p42_Hack_and_tweaks.py
@@ -0,0 +1,22 @@
+def check_guess(guess, answer):
+    global score
+    still_guessing = True
+    attemp = 0
+    while still_guessing and attemp < 3:
+        if guess.lower() == answer.lower():
+            print("Correct answer.")
+            score = score+1
+            still_guessing = False
+        else:
+            if attemp < 2:
+                guess = input('Sorry wrong answer.Try again. \n')
+            attemp = attemp+1
+    if attemp == 3:
+        print("The correct answer is "+answer+".")
+
+
+score = 0
+guess = input('Which one of these is a fish? \n \
+A) Whale\n B) Dolphin\n C) Shark\n D) Squid \nType A, B, C, or D \n')
+check_guess(guess, 'C')
+print('Your score is ' + str(score)+".")
