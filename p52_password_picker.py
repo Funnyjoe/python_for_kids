30e0c784b2421ca8c4ab7e4179205b206064a28b
Funnyjoe <38206938+Funnyjoe@users.noreply.github.com>
2020-11-02 12:20:17 +0800

    Add page 54-56

--- p52_password_picker.py
+++ p52_password_picker.py
@@ -8,9 +8,16 @@ nouns = ['apple', 'dinosaur', 'ball', 'toaster', 'goat', 'dragon',
          'hammer', 'duck', 'panda']
 
 print('Welcome to Password Picker!')
-adjective = random.choice(adjectives)
-noun = random.choice(nouns)
-number = random.randrange(0, 100)
-special_char = random.choice(string.punctuation)
-password = adjective+noun+str(number)+special_char
-print('Your new password is: %s ' % password)
+
+while True:
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randrange(0, 100)
+    special_char = random.choice(string.punctuation)
+
+    password = adjective+noun+str(number)+special_char
+    print('Your new password is: %s ' % password)
+
+    response = input('Would you like another password? Type y or n: \n')
+    if response == 'n':
+        break
